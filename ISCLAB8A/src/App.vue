a9a3b89092244f4b234323409b50109753ecb775
SebastianPool25 <jose.19070054@itsmotul.edu.mx>
2023-02-15 14:35:25 -0600

    Subida de Vue con tailwind

--- ISCLAB8A/src/App.vue
+++ ISCLAB8A/src/App.vue
@@ -1,11 +1,11 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+
+
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+    <img alt="Aqui deberia haber un logo :'(" class="logo" src="" width="125" height="125" />
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
